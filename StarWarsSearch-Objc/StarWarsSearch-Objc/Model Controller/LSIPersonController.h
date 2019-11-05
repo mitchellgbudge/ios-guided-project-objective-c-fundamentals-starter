287218ce73116022a7ddc623d64d9e7490180c10
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-05 11:30:45 -0700

    Stage networking method in model controller

---
+++ StarWarsSearch-Objc/StarWarsSearch-Objc/Model Controller/LSIPersonController.h
@@ -0,0 +1,17 @@
+//
+//  LSIPersonController.h
+//  StarWarsSearch-Objc
+//
+//  Created by Mitchell Budge on 11/5/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSIPersonController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StarWarsSearch-Objc/StarWarsSearch-Objc/Model Controller/LSIPersonController.m
@@ -0,0 +1,13 @@
+//
+//  LSIPersonController.m
+//  StarWarsSearch-Objc
+//
+//  Created by Mitchell Budge on 11/5/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIPersonController.h"
+
+@implementation LSIPersonController
+
+@end

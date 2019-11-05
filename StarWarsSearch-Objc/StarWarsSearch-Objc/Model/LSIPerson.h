eb0319ccf3790eafa763c278897216ba2b8c70ef
Mitchell Budge <mitchellbudge@gmail.com>
2019-11-05 10:31:31 -0700

    Write JSON decoding unit test

---
+++ StarWarsSearch-Objc/StarWarsSearch-Objc/Model/LSIPerson.h
@@ -0,0 +1,18 @@
+//
+//  LSIPerson.h
+//  StarWarsSearch-Objc
+//
+//  Created by Mitchell Budge on 11/5/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LSIPerson : NSObject
+
+@property NSString *name;
+@property NSString *birthYear;
+@property NSString *height;
+@property NSString *eyeColor;
+
+@end
---
+++ StarWarsSearch-Objc/StarWarsSearch-Objc/Model/LSIPerson.m
@@ -0,0 +1,15 @@
+//
+//  LSIPerson.m
+//  StarWarsSearch-Objc
+//
+//  Created by Mitchell Budge on 11/5/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "LSIPerson.h"
+
+@implementation LSIPerson
+
+
+
+@end
